1b227b86c5f59857a19ad69827ea721a83b2575d
satishgupta07 <satishgupta.cse01@gmail.com>
2020-06-14 00:07:47 +0530

    hello world heroku server
    Signed-off-by: satishgupta07 <satishgupta.cse01@gmail.com>

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+
+const PORT = process.env.PORT || 4444
+
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
+
+app.listen(PORT, (req, res) => {
+    console.log(`started on http://localhost:${PORT}`)
+  }) 
